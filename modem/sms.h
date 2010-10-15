01416ebc04f6da9779d62119ae9894fc0e90db61
Pekka Pessi <Pekka.Pessi@nokia.com>
2010-10-15 15:37:44 +0300

    modem/sms: using telepathy coding style

--- modem/sms.h
+++ modem/sms.h
@@ -39,42 +39,45 @@ G_END_DECLS
 
 G_BEGIN_DECLS
 
-struct _ModemSMSServiceClass {
+struct _ModemSMSServiceClass
+{
   GObjectClass parent_class;
 };
 
-struct _ModemSMSService {
+struct _ModemSMSService
+{
   GObject parent;
   ModemSMSServicePrivate *priv;
 };
 
-GType modem_sms_service_get_type(void);
+GType modem_sms_service_get_type (void);
 
 /* TYPE MACROS */
 #define MODEM_TYPE_SMS_SERVICE                  \
-  (modem_sms_service_get_type())
-#define MODEM_SMS_SERVICE(obj)                                          \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj), MODEM_TYPE_SMS_SERVICE, ModemSMSService))
-#define MODEM_SMS_SERVICE_CLASS(klass)                                  \
-  (G_TYPE_CHECK_CLASS_CAST((klass), MODEM_TYPE_SMS_SERVICE, ModemSMSServiceClass))
-#define MODEM_IS_SMS_SERVICE(obj)                               \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj), MODEM_TYPE_SMS_SERVICE))
-#define MODEM_IS_SMS_SERVICE_CLASS(klass)                       \
-  (G_TYPE_CHECK_CLASS_TYPE((klass), MODEM_TYPE_SMS_SERVICE))
-#define MODEM_SMS_SERVICE_GET_CLASS(obj)                                \
+  (modem_sms_service_get_type ())
+#define MODEM_SMS_SERVICE (obj)                                          \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), MODEM_TYPE_SMS_SERVICE, ModemSMSService))
+#define MODEM_SMS_SERVICE_CLASS (klass)                                  \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), MODEM_TYPE_SMS_SERVICE, ModemSMSServiceClass))
+#define MODEM_IS_SMS_SERVICE (obj)                               \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MODEM_TYPE_SMS_SERVICE))
+#define MODEM_IS_SMS_SERVICE_CLASS (klass)                       \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), MODEM_TYPE_SMS_SERVICE))
+#define MODEM_SMS_SERVICE_GET_CLASS (obj)                                \
   (G_TYPE_INSTANCE_GET_CLASS ((obj), MODEM_TYPE_SMS_SERVICE, ModemSMSServiceClass))
 
 /* ---------------------------------------------------------------------- */
 
-typedef void ModemSMSConnectedHandler(ModemSMSService *, gpointer);
-typedef void ModemSMSDeliverHandler(ModemSMSService *, SMSGDeliver *, gpointer);
+typedef void ModemSMSConnectedHandler (ModemSMSService *, gpointer);
+typedef void ModemSMSDeliverHandler (ModemSMSService *,
+    SMSGDeliver *, gpointer);
 
-typedef void ModemSMSServiceReply(ModemSMSService *self,
+typedef void ModemSMSServiceReply (ModemSMSService *self,
   ModemRequest *request,
   GError const *error,
   gpointer user_data);
 
-typedef void ModemSMSServiceSendReply(ModemSMSService *self,
+typedef void ModemSMSServiceSendReply (ModemSMSService *self,
   ModemRequest *request,
   char const *message_id,
   GError const *error,
@@ -82,36 +85,36 @@ typedef void ModemSMSServiceSendReply(ModemSMSService *self,
 
 /* ---------------------------------------------------------------------- */
 
-gulong modem_sms_connect_to_connected(ModemSMSService *self,
+gulong modem_sms_connect_to_connected (ModemSMSService *self,
   ModemSMSConnectedHandler *user_function,
   gpointer user_data);
 
-gulong modem_sms_connect_to_deliver(ModemSMSService *self,
+gulong modem_sms_connect_to_deliver (ModemSMSService *self,
   ModemSMSDeliverHandler *user_function,
   gpointer user_data);
 
-gboolean modem_sms_service_connect(ModemSMSService *self, char const *);
-void modem_sms_service_disconnect(ModemSMSService *self);
+gboolean modem_sms_service_connect (ModemSMSService *self, char const *);
+void modem_sms_service_disconnect (ModemSMSService *self);
 
-GError *modem_sms_service_connection_error(ModemSMSService *self);
-gboolean modem_sms_service_is_connected(ModemSMSService const *self);
-gboolean modem_sms_service_is_connecting(ModemSMSService const *self);
+GError *modem_sms_service_connection_error (ModemSMSService *self);
+gboolean modem_sms_service_is_connected (ModemSMSService const *self);
+gboolean modem_sms_service_is_connecting (ModemSMSService const *self);
 
-guint64 modem_sms_service_time_connected(ModemSMSService const *self);
+guint64 modem_sms_service_time_connected (ModemSMSService const *self);
 
 /* ---------------------------------------------------------------------- */
 
-ModemRequest *modem_sms_set_sc_address(ModemSMSService *self,
+ModemRequest *modem_sms_set_sc_address (ModemSMSService *self,
     char const *address,
     ModemSMSServiceReply *reply,
     gpointer user_data);
 
-ModemRequest *modem_sms_set_srr(ModemSMSService *self,
+ModemRequest *modem_sms_set_srr (ModemSMSService *self,
     gboolean srr,
     ModemSMSServiceReply *reply,
     gpointer user_data);
 
-ModemRequest *modem_sms_request_send(ModemSMSService *self,
+ModemRequest *modem_sms_request_send (ModemSMSService *self,
   char const *to, char const *message,
   ModemSMSServiceSendReply *reply,
   gpointer user_data);
